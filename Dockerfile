cede3cec6b6c42c49b1c34950c53c05b5be946de
Canale <raffaelecanale@gmail.com>
2019-06-29 11:02:56 +0200

    Add aliases

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM node
 WORKDIR /etc/wx/
 
-COPY src/ src/
 COPY package*.json .babelrc ./
 
 # There exists a bug that requires to build twice
-RUN npm install && \
-    npm run build && \
+RUN npm install
+
+COPY src/ src/
+
+# There exists a bug that requires to build twice
+RUN npm run build && \
     npm run build
 
 COPY config.json ./
--- src/utils/paths.js
+++ src/utils/paths.js
@@ -1,9 +1,14 @@
 import path from 'path';
-
+import fs from 'fs';
 // TODO Use config for this stuff
 const ROOT = './public';
 
+let aliases = {};
+
 export function getFile(parent, name, domain) {
+    if (aliases[parent]) {
+        parent = aliases[parent];
+    }
     return path.join(
         ROOT,
         parent,
@@ -12,6 +17,9 @@ export function getFile(parent, name, domain) {
 }
 
 export function getBackupFile(parent, name, domain, version) {
+    if (aliases[parent]) {
+        parent = aliases[parent];
+    }
     return path.join(
         ROOT,
         parent,
@@ -21,4 +29,9 @@ export function getBackupFile(parent, name, domain, version) {
 
 export function isValidDomain(domain) {
     return domain && domain !== '' && !domain.includes('.');
+}
+
+const aliasesConfig = path.join(ROOT, 'aliases.json');
+if (fs.existsSync(aliasesConfig)) {
+    aliases = JSON.parse(fs.readFileSync(aliasesConfig, 'utf8'));
 }
